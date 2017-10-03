abc617f7f787a2fb07e54377409a6d62132622f1
Tyler <tyler.walser@yahoo.com>
2017-10-03 09:56:20 -0700

    Added Location Class & Serialization example

---
+++ BigfootGame/BigfootGame/Locations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bigfootgame
+{
+    public class Locations
+    {
+        //public string Email { get; set; }
+        //public bool Active { get; set; }
+        //public DateTime CreatedDate { get; set; }
+        //public IList<string> Roles { get; set; }
+    }
+}
--- BigfootGame/BigfootGame/Program.cs
+++ BigfootGame/BigfootGame/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 
 namespace BigfootGame
 {
@@ -7,6 +9,34 @@ namespace BigfootGame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+
+            //Serialize a Location
+            //Location location = new Location
+            //{
+                //Email = "james@example.com",
+                //Active = true,
+                //CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                //Roles = new List<string>
+                //{
+                //"User",
+                //"Admin"
+                //}
+            //};
+
+            //string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+            // {
+            //   "Email": "james@example.com",
+            //   "Active": true,
+            //   "CreatedDate": "2013-01-20T00:00:00Z",
+            //   "Roles": [
+            //     "User",
+            //     "Admin"
+            //   ]
+            // }
+
+            //Console.WriteLine(json);
+
         }
     }
 }
