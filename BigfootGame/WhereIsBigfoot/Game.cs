c26c73eff5903cc66d7bca8a6ab97f7b74c1d1cc
Erica Winberry <ericaw@tlgclass.com>
2017-10-05 08:56:15 -0700

    parser is taking in text and checking for correct verbs.

--- BigfootGame/WhereIsBigfoot/Game.cs
+++ BigfootGame/WhereIsBigfoot/Game.cs
@@ -9,189 +9,195 @@ using static System.Console;
 
 namespace WhereIsBigfoot
 {
-    class Game
-    {
-        // Console formatting
-        Title = "Where Is Bigfoot?";
-        CursorVisible = true;
+	class Game
+	{
+		public List<Location> locations;
+		public List<Item> items;
+		public List<Character> characters;
+		List<string> allowedVerbs = new List<string>() { "get", "go", "give", "use", "talk", "put", "help", "quit", "inventory" };
+		private string currentVerb;
+		private string currentNoun;
+		private string currentSubject;
+		bool running = true;
 
-        public List<Location> locations;
-        public List<Item> items;
-        public List<Character> characters;
-        List<string> allowedVerbs = new List<string>() { "get", "go", "give", "use", "talk", "put", "help", "quit", "inventory" };
-        private string currentVerb;
-        private string currentNoun;
-        private string currentSubject;
-        bool running = true;
+		public string CurrentVerb
+		{
+			get => this.currentVerb;
+			set => this.currentVerb = value;
+		}
 
-        public string CurrentVerb
-        {
-            get => this.currentVerb;
-            set => this.currentVerb = value;
-        }
+		public string CurrentNoun
+		{
+			get => this.currentNoun;
+			set => this.currentNoun = value;
+		}
 
-        public string CurrentNoun
-        {
-            get => this.currentNoun;
-            set => this.currentNoun = value;
-        }
+		public string CurrentSubject
+		{
+			get => this.currentSubject;
+			set => this.currentSubject = value;
+		}
 
-        public string CurrentSubject {
-            get => this.currentSubject;
-            set => this.currentSubject = value;
-        }
+		// Deserialize JSON from a file. 
+		public void LoadData(Game game)
+		{
+			string jsonLocationFile = @"../../locations.json";
+			string jsonItemFile = @"../../items.json";
+			string jsonCharacterFile = @"../../characters.json";
 
-        // Deserialize JSON from a file. 
-        public void LoadData(Game game)
-        {
-            string jsonLocationFile = @"../../locations.json";
-            string jsonItemFile = @"../../items.json";
-            string jsonCharacterFile = @"../../characters.json";
+			game.items = JsonConvert.DeserializeObject<List<Item>>(File.ReadAllText(jsonItemFile));
+			game.characters = JsonConvert.DeserializeObject<List<Character>>(File.ReadAllText(jsonCharacterFile));
+			game.locations = JsonConvert.DeserializeObject<List<Location>>(File.ReadAllText(jsonLocationFile));
 
-            game.items = JsonConvert.DeserializeObject<List<Item>>(File.ReadAllText(jsonItemFile));
-            game.characters = JsonConvert.DeserializeObject<List<Character>>(File.ReadAllText(jsonCharacterFile));
-            game.locations = JsonConvert.DeserializeObject<List<Location>>(File.ReadAllText(jsonLocationFile));
+			////Testing to make sure the objects are being de-serialized by writing them to the console.
+			//WriteLine("Locations:");
+			//foreach (Location location in game.locations)
+			//    WriteLine(location.Name);
 
-            ////Testing to make sure the objects are being de-serialized by writing them to the console.
-            //WriteLine("Locations:");
-            //foreach (Location location in game.locations)
-            //    WriteLine(location.Name);
+			//WriteLine("\nItems:");
+			//foreach (Item item in game.items)
+			//    WriteLine($"Item {item.Name} is in location {item.Location}");
 
-            //WriteLine("\nItems:");
-            //foreach (Item item in game.items)
-            //    WriteLine($"Item {item.Name} is in location {item.Location}");
+			//WriteLine("\nCharacters:");
+			//foreach (Character character in game.characters)
+			//WriteLine(character.CharacterName);
 
-            //WriteLine("\nCharacters:");
-            //foreach (Character character in game.characters)
-            //WriteLine(character.CharacterName);
+			// Go through each item and assign the item to the items dict in each location, 
+			// based on the location property of the item.
+			foreach (Item item in game.items)
+			{
+				string name = item.Location;
+				string key = item.Name;
 
-            // Go through each item and assign the item to the items dict in each location, 
-            // based on the location property of the item.
-            foreach (Item item in game.items)
-            {
-                string name = item.Location;
-                string key = item.Name;
+				foreach (Location location in game.locations)
+				{
+					if (location.Name == name)
+					{
+						location.Items.Add(key, item);
+						//WriteLine($"Assigned item key {key} on location {location.Name}");
+					}
+				}
+			}
 
-                foreach (Location location in game.locations)
-                {
-                    if (location.Name == name)
-                    {
-                        location.Items.Add(key, item);
-                        //WriteLine($"Assigned item key {key} on location {location.Name}");
-                    }
-                }
-            }
+			//// Test assignment of items by count of the items dict on the location.
+			//WriteLine("\nLocation Items count:");
+			//foreach (Location location in game.locations)
+			//WriteLine($"Location: {location.Name}, # of items: {location.Items.Count}");
 
-            //// Test assignment of items by count of the items dict on the location.
-            //WriteLine("\nLocation Items count:");
-            //foreach (Location location in game.locations)
-            //WriteLine($"Location: {location.Name}, # of items: {location.Items.Count}");
+			// Go through each character and assign the character to the character dict in each location, 
+			// based on the location property of the characger.
+			foreach (Character character in game.characters)
+			{
+				string name = character.Location;
+				string key = character.Name;
 
-            // Go through each character and assign the character to the character dict in each location, 
-            // based on the location property of the characger.
-            foreach (Character character in game.characters)
-            {
-                string name = character.Location;
-                string key = character.Name;
+				foreach (Location location in game.locations)
+				{
+					if (location.Name == name)
+					{
+						location.Characters.Add(key, character);
+						//WriteLine($"Assigned character with key {key} on location {location.Name}");
+					}
+				}
+			}
 
-                foreach (Location location in game.locations)
-                {
-                    if (location.Name == name)
-                    {
-                        location.Characters.Add(key, character);
-                        //WriteLine($"Assigned character with key {key} on location {location.Name}");
-                    }
-                }
-            }
+			//// Test assignment of characters by count of the characters dict on the location.
+			//WriteLine("\nLocation Characters count:");
+			//foreach (Location location in game.locations)
+			//WriteLine($"Location: {location.Name}, # of items: {location.Characters.Count}");
 
-            //// Test assignment of characters by count of the characters dict on the location.
-            //WriteLine("\nLocation Characters count:");
-            //foreach (Location location in game.locations)
-            //WriteLine($"Location: {location.Name}, # of items: {location.Characters.Count}");
+			// Test existence of exits dict by count of the characters array on the location.
+			//WriteLine("\nLocation Exits count:");
+			//foreach (Location location in game.locations)
+			//WriteLine($"Location has {location.Exits.Count}");
 
-            // Test existence of exits dict by count of the characters array on the location.
-            //WriteLine("\nLocation Exits count:");
-            //foreach (Location location in game.locations)
-            //WriteLine($"Location has {location.Exits.Count}");
+		}
 
-        }
+		public void ParseInput(string prompt)
+		{
+			string input = GetInput(prompt);
 
-        public void ParseInput(string prompt)
-        {
-            string input = GetInput(prompt);
+			if (IsValidInput(input))
+			{
+				string[] split = input.Split(default(string[]), 2, StringSplitOptions.RemoveEmptyEntries);
 
-            if (IsValidInput(input))
-            {
-                string[] split = input.Split(default(string[]), 2, StringSplitOptions.RemoveEmptyEntries);
+				string verb = split[0];
 
-                string verb = split[0];
+				if (allowedVerbs.Contains(verb))
+				{
+					this.CurrentVerb = verb;
+					WriteLine($"Current Verb: {this.currentVerb}");
+				}
+				else
+				{
+					// call help method?
+					WriteLine("I'm sorry, I didn't understand that. For a list of usable verbs, type \"help\".");
+					ParseInput(prompt);
+				}
+				if (split.Length == 2)
+				{
+					this.CurrentNoun = split[1];
+					WriteLine($"Current Noun: {this.currentNoun}");
+				}
+				if (verb == "quit")
+				{
+					this.running = false;
+				}
+			}
+			else
+			{
+				WriteLine("I'm afraid I didn't understand that.");
+				GetInput(prompt);
+			}
+		}
 
-                if (allowedVerbs.Contains(verb))
-                {
-                    this.CurrentVerb = verb;
-                    WriteLine($"Current Verb: {this.currentVerb}");
-                }
-                else
-                {
-                    // call help method.
-                    WriteLine("Bad input. Needs help method");
-                }
-                if (split.Length == 2)
-                {
-                    this.CurrentNoun = split[1];
-                    WriteLine($"Current Noun: {this.currentNoun}");
-                }
-                if (verb == "quit") {
-                    this.running = false;
-                }
-            }
-            else
-            {
-                WriteLine("I'm afraid I didn't understand that.");
-                GetInput(prompt);
-            }
-        }
+		//public void Parser(string input, Game game)
+		//{
 
-        //public void Parser(string input, Game game)
-        //{
+		//}
 
-        //}
+		//        read input(to lower)
+		// splits on “”
+		// checks first word vs list(verbs)
+		// – if in list of verbs
+		// – if not throw help
+		// checks second word vs list(nouns)
+		// – if action-able then do
+		// takes method
+		// validates verb
+		// returns(verb, noun)
 
-        //        read input(to lower)
-        // splits on “”
-        // checks first word vs list(verbs)
-        // – if in list of verbs
-        // – if not throw help
-        // checks second word vs list(nouns)
-        // – if action-able then do
-        // takes method
-        // validates verb
-        // returns(verb, noun)
+		// Console formatting
+		public void FormatConsole()
+		{
+			Console.Title = "Where Is Bigfoot?";
+			Console.CursorVisible = true;
+		}
 
-        static void Main(string[] args)
-        {
+		static void Main(string[] args)
+		{
 
-            Game game = new Game();
+			Game game = new Game();
+			game.FormatConsole();
 
-            game.LoadData(game);
-                
-            game.ParseInput("> ");
+			game.LoadData(game);
 
-        }
+			game.ParseInput("> ");
 
-        public static bool IsValidInput(string str)
-        {
-            Regex regexString = new Regex(@"[a-z\s']*$");
-            return regexString.IsMatch(str);
-        }
+		}
 
-        public static string GetInput(string prompt)
-        {
-            Console.Write(prompt);
-            string input = ReadLine();
-            input = "fish sticks and custard";
-            return input.ToLower().Trim();
-        }
+		public static bool IsValidInput(string str)
+		{
+			Regex regexString = new Regex(@"[a-z\s']*$");
+			return regexString.IsMatch(str);
+		}
 
-    }
+		public static string GetInput(string prompt)
+		{
+			Console.Write(prompt);
+			string input = ReadLine();
+			return input.ToLower().Trim();
+		}
+
+	}
 }
