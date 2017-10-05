16b435ca9b3b602f975e0572b6790592fca395ca
Erica Winberry <elaewin@gmail.com>
2017-10-04 22:06:47 -0700

    beating my head against null value errors.

--- BigfootGame/WhereIsBigfoot/Game.cs
+++ BigfootGame/WhereIsBigfoot/Game.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using Newtonsoft.Json;
-using System.Data;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 using static System.Console;
 
 
@@ -10,10 +11,31 @@ namespace WhereIsBigfoot
 {
     class Game
     {
-        //
         public List<Location> locations;
         public List<Item> items;
         public List<Character> characters;
+        List<string> allowedVerbs = new List<string>() { "get", "go", "give", "use", "talk", "put", "help", "quit", "inventory" };
+        private string currentVerb;
+        private string currentNoun;
+        private string currentSubject;
+        bool running = true;
+
+        public string CurrentVerb
+        {
+            get => this.currentVerb;
+            set => this.currentVerb = value;
+        }
+
+        public string CurrentNoun
+        {
+            get => this.currentNoun;
+            set => this.currentNoun = value;
+        }
+
+        public string CurrentSubject {
+            get => this.currentSubject;
+            set => this.currentSubject = value;
+        }
 
         // Deserialize JSON from a file. 
         public void LoadData(Game game)
@@ -30,23 +52,26 @@ namespace WhereIsBigfoot
             //WriteLine("Locations:");
             //foreach (Location location in game.locations)
             //    WriteLine(location.Name);
-            
+
             //WriteLine("\nItems:");
             //foreach (Item item in game.items)
             //    WriteLine($"Item {item.Name} is in location {item.Location}");
-            
+
             //WriteLine("\nCharacters:");
             //foreach (Character character in game.characters)
-                //WriteLine(character.CharacterName);
+            //WriteLine(character.CharacterName);
 
             // Go through each item and assign the item to the items dict in each location, 
             // based on the location property of the item.
-            foreach(Item item in game.items) {
+            foreach (Item item in game.items)
+            {
                 string name = item.Location;
                 string key = item.Name;
 
-                foreach(Location location in game.locations) {
-                    if (location.Name == name) {
+                foreach (Location location in game.locations)
+                {
+                    if (location.Name == name)
+                    {
                         location.Items.Add(key, item);
                         //WriteLine($"Assigned item key {key} on location {location.Name}");
                     }
@@ -56,8 +81,8 @@ namespace WhereIsBigfoot
             //// Test assignment of items by count of the items dict on the location.
             //WriteLine("\nLocation Items count:");
             //foreach (Location location in game.locations)
-                //WriteLine($"Location: {location.Name}, # of items: {location.Items.Count}");
-            
+            //WriteLine($"Location: {location.Name}, # of items: {location.Items.Count}");
+
             // Go through each character and assign the character to the character dict in each location, 
             // based on the location property of the characger.
             foreach (Character character in game.characters)
@@ -78,18 +103,66 @@ namespace WhereIsBigfoot
             //// Test assignment of characters by count of the characters dict on the location.
             //WriteLine("\nLocation Characters count:");
             //foreach (Location location in game.locations)
-                //WriteLine($"Location: {location.Name}, # of items: {location.Characters.Count}");
+            //WriteLine($"Location: {location.Name}, # of items: {location.Characters.Count}");
 
             // Test existence of exits dict by count of the characters array on the location.
             //WriteLine("\nLocation Exits count:");
             //foreach (Location location in game.locations)
-                //WriteLine($"Location has {location.Exits.Count}");
+            //WriteLine($"Location has {location.Exits.Count}");
 
         }
 
-        public void GetInput() {
-            
+        public void ParseInput(string prompt)
+        {
+            string input = GetInput(prompt);
+
+            if (IsValidInput(input))
+            {
+                string[] split = input.Split(default(string[]), 2, StringSplitOptions.RemoveEmptyEntries);
+
+                string verb = split[0];
+
+                if (allowedVerbs.Contains(verb))
+                {
+                    this.CurrentVerb = verb;
+                    WriteLine($"Current Verb: {this.currentVerb}");
+                }
+                else
+                {
+                    // call help method.
+                    WriteLine("Bad input.");
+                }
+                if (split.Length == 2)
+                {
+                    this.CurrentNoun = split[1];
+                    WriteLine($"Current Noun: {this.currentNoun}");
+                }
+                if (verb == "quit") {
+                    this.running = false;
+                }
+            }
+            else
+            {
+                WriteLine("I'm afraid I didn't understand that.");
+                GetInput(prompt);
+            }
         }
+
+        //public void Parser(string input, Game game)
+        //{
+
+        //}
+
+        //        read input(to lower)
+        // splits on “”
+        // checks first word vs list(verbs)
+        // – if in list of verbs
+        // – if not throw help
+        // checks second word vs list(nouns)
+        // – if action-able then do
+        // takes method
+        // validates verb
+        // returns(verb, noun)
 
         static void Main(string[] args)
         {
@@ -97,7 +170,24 @@ namespace WhereIsBigfoot
             Game game = new Game();
 
             game.LoadData(game);
+                
+            GetInput("> ");
 
         }
+
+        public static bool IsValidInput(string str)
+        {
+            Regex regexString = new Regex(@"[a-z\s']*$");
+            return regexString.IsMatch(str);
+        }
+
+        public static string GetInput(string prompt)
+        {
+            string input = "placeholder";
+            Console.Write(prompt);
+            input = ReadLine();
+            return input;
+        }
+
     }
 }
